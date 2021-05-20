fb1b41bcf2408a7c3e534e61ef9cca05e6eb50db
Timothy Martin <instanttim@users.noreply.github.com>
2021-05-20 16:20:05 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ if [ "$1" = 'remove' ]
 		rm ~/.oh-my-zsh/custom/aliases.zsh
 		rm ~/.zshrc
 else
+	./install.sh remove
 	ln -s $PWD/instanttim.zsh-theme ~/.oh-my-zsh/custom/themes/instanttim.zsh-theme 
 	ln -s $PWD/aliases.zsh ~/.oh-my-zsh/custom/aliases.zsh
 	ln -s $PWD/zshrc ~/.zshrc
