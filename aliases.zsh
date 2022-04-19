a74d66b84af3d2f890acc276645617e6d28476b8
Timothy Martin <tmartin@apple.com>
2022-04-19 19:23:23 +0000

    Added config editing alias.

--- aliases.zsh
+++ aliases.zsh
@@ -3,3 +3,4 @@ alias brewup="brew update;brew upgrade"
 alias pa="php artisan"
 alias ffile='find . -type f -name'
 alias fdir='find . -type d -name'
+alias zshconfig="nova ~/Documents/Development/zsh-config"
