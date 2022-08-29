af91b7c309feef5e8be731e1ab9f2afbee1874ba
Timothy Martin <tim@goldenhome.co>
2022-08-29 15:44:04 -0600

    homebrew path and cleanup

--- aliases.zsh
+++ aliases.zsh
@@ -1,5 +1,5 @@
 alias du="du -h"
-alias brewup="brew update;brew upgrade"
+alias brewup="brew update;brew upgrade;brew cleanup"
 alias pa="php artisan"
 alias ffile='find . -type f -name'
 alias fdir='find . -type d -name'
