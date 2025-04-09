d021225ea2c5e0a296c96cad97b661cde6727c17
Timothy Martin <tmartin@apple.com>
2025-04-09 11:36:04 -0600

    renamed script, updated readme.

--- set-up.sh
+++ set-up.sh
@@ -1,11 +1,10 @@
 #!/bin/zsh
 
 # (2) handle the command line argument we were given
-if [ "$1" = 'prepare' ]
+if [ "$1" = 'install' ]
 	then
-		sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+		sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/set-up.sh -O -)"
 		git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-		git clone https://github.com/zsh-users/zsh-history-substring-search $ZSH_CUSTOM/plugins/zsh-history-substring-search
 		git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 elif [ "$1" = 'remove' ]
 	then
@@ -20,7 +19,7 @@ elif [ "$1" = 'unlink' ]
 		cp $PWD/zshrc ~/.zshrc	
 elif [ "$1" = 'link' ]
 	then
-		./install.sh remove
+		./set-up.sh remove
 		echo "Linking zsh-theme, aliases, and zshrc files."
 		ln -s $PWD/instanttim.zsh-theme ~/.oh-my-zsh/custom/themes/instanttim.zsh-theme 
 		ln -s $PWD/aliases.zsh ~/.oh-my-zsh/custom/aliases.zsh
@@ -29,8 +28,8 @@ else
 	if [ -z $ZSH ]
 		then
 			echo "Installing…"
-			./install.sh prepare
-			./install.sh link
+			./set-up.sh prepare
+			./set-up.sh link
 	else
 		echo "REQUIRES: prepare, link, unlink, or remove."
 	fi
