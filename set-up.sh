68f3703b8610b5fb2cc8fc3afe9766572c26bf9a
Timothy Martin <instanttim@users.noreply.github.com>
2026-03-22 15:55:19 -0600

    Update set-up.sh

--- set-up.sh
+++ set-up.sh
@@ -3,7 +3,7 @@
 # (2) handle the command line argument we were given
 if [ "$1" = 'install' ]
 	then
-		sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/set-up.sh -O -)"
+		sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 		git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 		git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 elif [ "$1" = 'remove' ]
@@ -28,7 +28,7 @@ else
 	if [ -z $ZSH ]
 		then
 			echo "Installing…"
-			./set-up.sh prepare
+			./set-up.sh install
 			./set-up.sh link
 	else
 		echo "REQUIRES: prepare, link, unlink, or remove."
